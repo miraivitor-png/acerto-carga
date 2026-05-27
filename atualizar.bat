061c3b7a647d3624d99b8d69e7a96db321bda01d
miraivitor-png <mirai.vitor@gmail.com>
2026-05-27 11:18:28 -0300

    atualizacao 27/05/2026 11:18:28,13

--- atualizar.bat
+++ atualizar.bat
@@ -9,18 +9,18 @@ cd /d C:\Users\Vitor\Downloads\Acerto_Web_Github
 
 echo Copiando arquivos novos da pasta Downloads...
 
-if exist "%USERPROFILE%\Downloads\app_motorista.html" (
-    copy /Y "%USERPROFILE%\Downloads\app_motorista.html" index.html > nul
-    echo   OK - app_motorista.html copiado como index.html
-)
 if exist "%USERPROFILE%\Downloads\index.html" (
     copy /Y "%USERPROFILE%\Downloads\index.html" index.html > nul
     echo   OK - index.html copiado
+) else (
+    echo   AVISO - index.html nao encontrado em Downloads
 )
+
 if exist "%USERPROFILE%\Downloads\manifest.json" (
     copy /Y "%USERPROFILE%\Downloads\manifest.json" . > nul
     echo   OK - manifest.json copiado
 )
+
 if exist "%USERPROFILE%\Downloads\service-worker.js" (
     copy /Y "%USERPROFILE%\Downloads\service-worker.js" . > nul
     echo   OK - service-worker.js copiado
